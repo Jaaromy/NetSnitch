a653e03f35ab5883d3b70750c01fff365ca55f17
Jaaromy Zierse <jaaromy.zierse@gmail.com>
2015-08-15 15:18:57 -0600

    Initial performance project.

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using PerformanceModule;
+using Utilities;
 
 namespace ConsoleClient
 {
@@ -6,7 +12,34 @@ namespace ConsoleClient
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			//var currentProcess = Process.GetCurrentProcess();
+
+			//currentProcess.Dump();
+
+			Performance.Start();
+
+			//ShowAllProcesses();
+
+
+		}
+
+		private static void ShowAllProcesses()
+		{
+			var allProcesses = Process.GetProcesses().OrderBy(p => p.ProcessName).ToList();
+
+			var maxNameLength = allProcesses.OrderByDescending(ap => ap.ProcessName.Length).Select(ap => ap.ProcessName.Length).First();
+
+			var header = $"Number  {"Name".PadRight(maxNameLength)} {"ID",-5} {"Memory",-6}";
+
+			Console.WriteLine(header.UnderlineWith('='));
+
+
+			for (var i = 0; i < allProcesses.Count; i++)
+			{
+				var name = allProcesses[i].ProcessName.PadRight(maxNameLength);
+
+				Console.WriteLine($"{i + 1,-7} {name} {allProcesses[i].Id,-5} {allProcesses[i].NonpagedSystemMemorySize64,-6}");
+			}
 		}
 	}
 }
---
+++ Utilities/Extensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+
+namespace Utilities
+{
+	public static class Extensions
+	{
+		public static void Dump(this object obj)
+		{
+			foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(obj))
+			{
+				var name = descriptor.Name;
+
+				object value;
+
+				try
+				{
+					value = descriptor.GetValue(obj);
+
+				}
+				catch (Exception exception)
+				{
+					value = exception.GetType();
+				}
+				Console.WriteLine($"{name}={value}");
+			}
+
+		}
+
+		public static string UnderlineWith(this string str, char underlineChar)
+		{
+			var retVal = new StringBuilder();
+			retVal.Append(str);
+			retVal.Append(Environment.NewLine);
+			retVal.Append(new string(underlineChar, str.Length));
+
+			return retVal.ToString();
+		}
+	}
+}
